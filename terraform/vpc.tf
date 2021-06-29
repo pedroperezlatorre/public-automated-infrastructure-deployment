bf045732a4805b03a6820f44cde3ece6d360a470
Guillermo López Barranco <guillermo.lopez@ibm.com>
2021-06-29 14:47:56 +0200

    Fix variable warning III

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -46,7 +46,7 @@ resource ibm_is_vpc_address_prefix subnet_prefix {
   name  = "${var.unique_id}-prefix-zone-${count.index + 1}"
   zone  = "${var.ibm_region}-${(count.index % 3) + 1}"
   vpc   = ibm_is_vpc.vpc.id
-  cidr  = "${element(var.cidr_blocks, count.index)}"
+  cidr  = element(var.cidr_blocks, count.index)
 }
 
 ##############################################################################
