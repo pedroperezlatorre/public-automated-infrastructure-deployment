3c2a59473228c35c39c1bea7225aa6d4fd274a79
Pedro-Perez-Latorre <pedro.perez.latorre@ibm.com>
2021-07-14 11:16:55 +0200

    Fixing Watson Discovery vol2

--- terraform/watsondiscovery.tf
+++ terraform/watsondiscovery.tf
@@ -1,5 +1,5 @@
 resource "ibm_resource_instance" "w_discovery_instance" {
-  name              = "${var.unique_id}-w_discovery"
+  name              = "${var.unique_id}-w-discovery"
   service           = "discovery"
   plan              = var.w_discovery_plan
   location          = var.ibm_region
