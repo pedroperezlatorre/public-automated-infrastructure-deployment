6f92cb718c0b8a538289dcea65cedc4616b6813a
Guillermo López Barranco <guillermo.lopez@ibm.com>
2021-07-16 14:13:10 +0200

    Modularize DB2

---
+++ terraform/db2/variables.tf
@@ -0,0 +1,4 @@
+variable "unique_id" {}
+variable "db2_plan" {}
+variable "ibm_region" {}
+variable "db2_key_role" {}
--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,11 @@ module "cloudant" {
   ibm_region        = var.ibm_region
   cloudant_key_role = var.cloudant_key_role
 }
+
+module "db2" {
+  source            = "./db2"
+  unique_id         = var.unique_id
+  cloudant_plan     = var.db2_plan
+  ibm_region        = var.ibm_region
+  cloudant_key_role = var.db2_key_role
+}
