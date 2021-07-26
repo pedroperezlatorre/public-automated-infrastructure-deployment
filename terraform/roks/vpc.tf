7b1670359bbe6da5ae7d72ba1c6833d42760d50f
Guillermo López Barranco <guillermo.lopez@ibm.com>
2021-07-26 13:54:56 +0200

    Reference resource_group id in VPC

--- terraform/roks/vpc.tf
+++ terraform/roks/vpc.tf
@@ -78,7 +78,7 @@ resource ibm_is_subnet subnet {
   vpc             = ibm_is_vpc.vpc.id
   zone            = "${var.ibm_region}-${count.index + 1}"
   #resource_group  = "${data.ibm_schematics_output.groups_output.output_values.resource_group_id}"
-  resource_group  = ibm_resource_group.resource_group.id
+  resource_group  = var.rg_id
   ipv4_cidr_block = element(ibm_is_vpc_address_prefix.subnet_prefix.*.cidr, count.index)
   public_gateway  = element(ibm_is_public_gateway.gateway.*.id, count.index)
   network_acl     = ibm_is_network_acl.network_acl.id
