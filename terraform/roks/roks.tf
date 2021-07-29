a372355ec54c0a10e444dc0bdbbc2762085e752d
Guillermo López Barranco <guillermo.lopez@ibm.com>
2021-07-29 12:16:19 +0200

    Revert "Iteration for zone creation"
    This reverts commit aaa0a20937983d66a737499cb6a2a1bf11340c2b.

--- terraform/roks/roks.tf
+++ terraform/roks/roks.tf
@@ -31,23 +31,16 @@ resource "ibm_container_vpc_cluster" "cluster" {
  entitlement       = "cloud_pak"
  cos_instance_crn  = ibm_resource_instance.cos.id
 
-
-  zones {
-    count = 3
-    subnet_id = element(ibm_is_subnet.subnet.*.id, count.index)
-    name      = "${var.ibm_region}-${count.index + 1}"
-  }
-
-#  zones {
-# #  subnet_id = element(data.ibm_schematics_output.vpc_workspace.output_values.subnet_ids, 0)
-#    subnet_id = ibm_is_subnet.subnet.0.id
-#    name      = "${var.ibm_region}-1"
-#  }
-#  zones {
-# #   subnet_id = element(data.ibm_schematics_output.vpc_workspace.output_values.subnet_ids, 1)
-#    subnet_id = ibm_is_subnet.subnet.1.id
-#    name      = "${var.ibm_region}-2"
-#  }
+ zones {
+#  subnet_id = element(data.ibm_schematics_output.vpc_workspace.output_values.subnet_ids, 0)
+   subnet_id = ibm_is_subnet.subnet.0.id
+   name      = "${var.ibm_region}-1"
+ }
+ zones {
+#   subnet_id = element(data.ibm_schematics_output.vpc_workspace.output_values.subnet_ids, 1)
+   subnet_id = ibm_is_subnet.subnet.1.id
+   name      = "${var.ibm_region}-2"
+ }
 #   zones {
 # #   subnet_id = element(data.ibm_schematics_output.vpc_workspace.output_values.subnet_ids, 2)
 #     subnet_id = ibm_is_subnet.subnet.2.id
