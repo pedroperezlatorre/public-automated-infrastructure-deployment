c186317746369b822628f76d4b016d9b8cefb4a3
Guillermo López Barranco <guillermo.lopez@ibm.com>
2021-07-29 12:20:12 +0200

    Set ROKS with 3 zones

--- terraform/roks/roks.tf
+++ terraform/roks/roks.tf
@@ -31,25 +31,21 @@ resource "ibm_container_vpc_cluster" "cluster" {
  entitlement       = "cloud_pak"
  cos_instance_crn  = ibm_resource_instance.cos.id
 
- zones {
-#  subnet_id = element(data.ibm_schematics_output.vpc_workspace.output_values.subnet_ids, 0)
-   subnet_id = ibm_is_subnet.subnet.0.id
-   name      = "${var.ibm_region}-1"
- }
- zones {
-#   subnet_id = element(data.ibm_schematics_output.vpc_workspace.output_values.subnet_ids, 1)
-   subnet_id = ibm_is_subnet.subnet.1.id
-   name      = "${var.ibm_region}-2"
- }
-#   zones {
-# #   subnet_id = element(data.ibm_schematics_output.vpc_workspace.output_values.subnet_ids, 2)
-#     subnet_id = ibm_is_subnet.subnet.2.id
-#     name      = "${var.ibm_region}-3"
-#   }
-}
+# Zone 1
+  zones {
+    subnet_id = ibm_is_subnet.subnet.0.id
+    name      = "${var.ibm_region}-1"
+  }
 
-# resource "kubernetes_namespace" "example" {
-#   metadata {
-#     name = "terraform-example-namespace"
-#   }
-# }
+# Zone 2
+  zones {
+    subnet_id = ibm_is_subnet.subnet.1.id
+    name      = "${var.ibm_region}-2"
+  }
+
+# Zone 3
+  zones {
+    subnet_id = ibm_is_subnet.subnet.2.id
+    name      = "${var.ibm_region}-3"
+  }
+}
