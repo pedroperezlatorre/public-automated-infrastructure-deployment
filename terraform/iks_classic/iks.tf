bafbb41763689bd5c397f7052fe53c15fff2cf39
Guillermo López Barranco <guillermo.lopez@ibm.com>
2021-08-24 13:17:41 +0200

    Comment VPC cluster

--- terraform/iks_classic/iks.tf
+++ terraform/iks_classic/iks.tf
@@ -2,32 +2,32 @@
 # Provison IKS Classic Cluster
 ##############################################################################
 
-resource "ibm_container_vpc_cluster" "cluster" {
- name              = "${var.unique_id}-iks-vpc"
- vpc_id            = ibm_is_vpc.vpc.id
- flavor            = var.machine_type
- worker_count      = var.worker_count
- resource_group_id = var.rg_id
- kube_version      = var.kube_version
+# resource "ibm_container_vpc_cluster" "cluster" {
+#  name              = "${var.unique_id}-iks-vpc"
+#  vpc_id            = ibm_is_vpc.vpc.id
+#  flavor            = var.machine_type
+#  worker_count      = var.worker_count
+#  resource_group_id = var.rg_id
+#  kube_version      = var.kube_version
  
-# Zone 1
-  zones {
-    subnet_id = ibm_is_subnet.subnet.0.id
-    name      = "${var.ibm_region}-1"
-  }
+# # Zone 1
+#   zones {
+#     subnet_id = ibm_is_subnet.subnet.0.id
+#     name      = "${var.ibm_region}-1"
+#   }
 
-# Zone 2
-  zones {
-    subnet_id = ibm_is_subnet.subnet.1.id
-    name      = "${var.ibm_region}-2"
-  }
+# # Zone 2
+#   zones {
+#     subnet_id = ibm_is_subnet.subnet.1.id
+#     name      = "${var.ibm_region}-2"
+#   }
 
-# Zone 3
-  zones {
-    subnet_id = ibm_is_subnet.subnet.2.id
-    name      = "${var.ibm_region}-3"
-  }
-}
+# # Zone 3
+#   zones {
+#     subnet_id = ibm_is_subnet.subnet.2.id
+#     name      = "${var.ibm_region}-3"
+#   }
+# }
 
 resource "ibm_container_cluster" "cluster" {
   name               = "${var.unique_id}-iks-classic"
