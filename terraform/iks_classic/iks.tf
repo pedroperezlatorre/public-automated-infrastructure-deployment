4a6cdff3c6ba316beb21f09e0f68c3af105ebe25
Guillermo López Barranco <guillermo.lopez@ibm.com>
2021-08-26 14:06:47 +0200

    Comment old IKS code

--- terraform/iks_classic/iks.tf
+++ terraform/iks_classic/iks.tf
@@ -29,22 +29,22 @@
 #   }
 # }
 
-resource "ibm_container_cluster" "cluster" {
-  name               = "${var.unique_id}-iks-classic"
-  datacenter         = var.cluster_datacenter
-  resource_group_id  = var.rg_id
-  machine_type       = var.machine_type
-  hardware           = var.cluster_hardware
-  kube_version       = var.kube_version
-  update_all_workers = true
-  gateway_enabled    = false
-  private_service_endpoint = true
+# resource "ibm_container_cluster" "cluster" {
+#   name               = "${var.unique_id}-iks-classic"
+#   datacenter         = var.cluster_datacenter
+#   resource_group_id  = var.rg_id
+#   machine_type       = var.machine_type
+#   hardware           = var.cluster_hardware
+#   kube_version       = var.kube_version
+#   update_all_workers = true
+#   gateway_enabled    = false
+#   private_service_endpoint = true
 
-  default_pool_size = var.default_pool_size
+#   default_pool_size = var.default_pool_size
 
-  private_vlan_id = var.cluster_private_vlan
-  public_vlan_id  = var.cluster_public_vlan
-}
+#   private_vlan_id = var.cluster_private_vlan
+#   public_vlan_id  = var.cluster_public_vlan
+# }
 
 resource "ibm_container_cluster" "cluster" {
   name                     = "${var.unique_id}-iks-classic"
