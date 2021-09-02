19305b040a3c3f3b08a2c8875720de2414c737d8
Guillermo López Barranco <guillermo.lopez@ibm.com>
2021-09-02 10:57:52 +0200

    Conditional workerPool deployment

--- terraform/roks_classic/worker_pool.tf
+++ terraform/roks_classic/worker_pool.tf
@@ -1,4 +1,6 @@
 resource "ibm_container_worker_pool" "workerpool1" {
+  count = var.worker_num > 0 ? 1 : 0
+
   worker_pool_name = var.wp_worker_pool_name
   cluster          = ibm_container_cluster.cluster.id
 #   depends_on       = ["ibm_container_cluster.tfer--guilletest-002D-roks-002D-classic"]
