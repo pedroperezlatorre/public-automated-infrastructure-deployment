fcaa75a13a1a8aec18f1a17675ec69713a64a28d
Guillermo López Barranco <guillermo.lopez@ibm.com>
2021-09-02 11:04:09 +0200

    access worker_pool with index

--- terraform/roks_classic/worker_pool_zone.tf
+++ terraform/roks_classic/worker_pool_zone.tf
@@ -1,9 +1,9 @@
 resource "ibm_container_worker_pool_zone_attachment" "workerpool1_zone_attach" {
   count = var.worker_num > 0 ? 1 : 0
-  
+
   cluster         = ibm_container_cluster.cluster.id
   private_vlan_id = var.cluster_private_vlan
   public_vlan_id  = var.cluster_public_vlan
-  worker_pool     = ibm_container_worker_pool.workerpool1.id
+  worker_pool     = ibm_container_worker_pool.workerpool1[0].id
   zone            = var.cluster_datacenter
 }
