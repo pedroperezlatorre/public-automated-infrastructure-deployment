ab925debd46d48c42b18143dfe2ff0c92cb7ad97
Guillermo López Barranco <guillermo.lopez@ibm.com>
2021-09-03 13:51:41 +0200

    Increase timeouts

--- terraform/roks_classic/worker_pool_zone.tf
+++ terraform/roks_classic/worker_pool_zone.tf
@@ -6,4 +6,10 @@ resource "ibm_container_worker_pool_zone_attachment" "workerpool1_zone_attach" {
   public_vlan_id  = var.cluster_public_vlan
   worker_pool     = element(split("/", ibm_container_worker_pool.workerpool1[0].id), 1)
   zone            = var.cluster_datacenter
+
+  timeouts {
+    create = "90m"
+    update = "3h"
+    delete = "30m"
+  }
 }
