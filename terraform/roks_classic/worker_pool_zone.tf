b878c69a0b751091dedb777af8bc464ffb5b29f1
Guillermo López Barranco <guillermo.lopez@ibm.com>
2021-09-03 12:25:07 +0200

    Fix ibm_container_worker_pool_zone_attachment with element-split

--- terraform/roks_classic/worker_pool_zone.tf
+++ terraform/roks_classic/worker_pool_zone.tf
@@ -4,6 +4,6 @@ resource "ibm_container_worker_pool_zone_attachment" "workerpool1_zone_attach" {
   cluster         = ibm_container_cluster.cluster.id
   private_vlan_id = var.cluster_private_vlan
   public_vlan_id  = var.cluster_public_vlan
-  worker_pool     = ibm_container_worker_pool.workerpool1[0].id
+  worker_pool     = element(split("/", ibm_container_worker_pool.workerpool1[0].id), 1)
   zone            = var.cluster_datacenter
 }
