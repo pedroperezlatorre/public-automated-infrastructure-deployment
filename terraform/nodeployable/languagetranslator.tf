e6bc5130004094ab5ca705dce7072e84956d21be
Pedro-Perez-Latorre <pedro.perez.latorre@ibm.com>
2021-09-03 11:16:44 +0200

    Translator - service credentials

--- terraform/nodeployable/languagetranslator.tf
+++ terraform/nodeployable/languagetranslator.tf
@@ -5,3 +5,23 @@ resource "ibm_resource_instance" "language_translator_instance" {
   location          = var.ibm_region
   resource_group_id = ibm_resource_group.resource_group.id
 }
+
+
+##############################################################################
+# Service Credentials
+##############################################################################
+
+resource "ibm_resource_key" "translator_key" {
+  name                 = "${ibm_resource_instance.translator_instance.name}-key"
+  role                 = var.language_translator_role
+  resource_instance_id = ibm_resource_instance.language_translator_instance.id
+}
+
+resource "ibm_container_bind_service" "translator_service_binding" {
+  cluster_name_id       = ibm_container_cluster.cluster.id
+  service_instance_name = ibm_resource_instance.language_translator_instance.name
+  namespace_id          = var.namespace
+  resource_group_id     = var.resource_group_id
+  key                   = ibm_resource_key.language_translator_key.name
+}
+##############################################################################
