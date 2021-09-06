609ded462b9a370df91eea7930b154b49672535c
Pedro-Perez-Latorre <pedro.perez.latorre@ibm.com>
2021-09-06 15:13:23 +0200

    Fixing again Language Translator Binding

--- terraform/languagetranslator.tf
+++ terraform/languagetranslator.tf
@@ -1,5 +1,5 @@
 resource "ibm_resource_instance" "language_translator_instance" {
-  name              = "${var.unique_id}-language_translator"
+  name              = "${var.unique_id}-language-translator"
   service           = "language-translator"
   plan              = var.language_translator_plan
   location          = var.ibm_region
