f423764474b0972727ba81724330e6c924d41d47
Guillermo López Barranco <guillermo.lopez@ibm.com>
2021-09-06 12:21:43 +0200

    Revert "disable_public_alb2"
    This reverts commit 45770adb642d091016ac6bfd12db8c694222a7e5.

--- terraform/roks_classic/worker_pool_zone.tf
+++ terraform/roks_classic/worker_pool_zone.tf
@@ -13,18 +13,3 @@ resource "ibm_container_worker_pool_zone_attachment" "workerpool1_zone_attach" {
     delete = "30m"
   }
 }
-
-resource "ibm_container_alb" "disable_public_alb2" {
-  count = var.wp_deploy ? 1 : 0
-
-  depends_on = [
-    ibm_container_cluster.cluster,
-    ibm_container_worker_pool_zone_attachment.zone
-  ]
-
-  alb_id = [
-    for alb in ibm_container_cluster.cluster.albs:
-      alb.id if alb.alb_type == "public" && length(regexall("alb2", alb.id)) > 0
-  ][0]
-  disable_deployment = true
-}
