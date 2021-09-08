704185c5e2ecdb9a1bfc582f6a89eb97feaa4c94
Guillermo López Barranco <guillermo.lopez@ibm.com>
2021-09-08 14:02:34 +0200

    Container Registry: set plan upgrade

--- terraform/container_registry.tf
+++ terraform/container_registry.tf
@@ -1,34 +1,44 @@
-resource "ibm_resource_instance" "registry" {
-  depends_on = [ibm_resource_group.resource_group]
+# resource "ibm_resource_instance" "registry" {
+#   depends_on = [ibm_resource_group.resource_group]
 
-   name              = "${var.unique_id}-registry"
-   service           = "container-registry"
-   plan              = var.registry_plan
-   location          = var.ibm_region
-   resource_group_id = ibm_resource_group.resource_group.id
+#    name              = "${var.unique_id}-registry"
+#    service           = "container-registry"
+#    plan              = var.registry_plan
+#    location          = var.ibm_region
+#    resource_group_id = ibm_resource_group.resource_group.id
   
-  timeouts {
-    create = "15m"
-    update = "15m"
-    delete = "15m"
+#   timeouts {
+#     create = "15m"
+#     update = "15m"
+#     delete = "15m"
+#   }
+#  }
+
+#  resource "ibm_resource_key" "registry_key" {
+#    name                 = "${ibm_resource_instance.registry.name}-key"
+#    role                 = var.registry_key_role
+#    resource_instance_id = ibm_resource_instance.registry.id
+#  }
+
+# # Pass credentials to K8S
+# ######################################################
+#  resource "ibm_container_bind_service" "registry_service_binding" {
+#      depends_on = [kubernetes_namespace.prod]
+#      #   count = length(var.environments)
+#      cluster_name_id       = module.roks_classic.cluster_id
+#      service_instance_name = ibm_resource_instance.registry.name
+#      namespace_id          = var.unique_id
+#      resource_group_id     = ibm_resource_group.resource_group.id
+#      key                   = ibm_resource_key.registry_key.name
+# }
+# ######################################################
+
+resource "null_resource" "registry" {
+  provisioner "local-exec" {
+    command = "ibmcloud login --apikey $IBMCLOUD_API_KEY -a \"https://cloud.ibm.com\" -r $REGION && (echo \"y\" | ibmcloud cr plan-upgrade standard)"
+    environment = {
+      IBMCLOUD_API_KEY = var.ibmcloud_api_key
+      REGION           = var.region
+    }
   }
- }
-
- resource "ibm_resource_key" "registry_key" {
-   name                 = "${ibm_resource_instance.registry.name}-key"
-   role                 = var.registry_key_role
-   resource_instance_id = ibm_resource_instance.registry.id
- }
-
-# Pass credentials to K8S
-######################################################
- resource "ibm_container_bind_service" "registry_service_binding" {
-     depends_on = [kubernetes_namespace.prod]
-     #   count = length(var.environments)
-     cluster_name_id       = module.roks_classic.cluster_id
-     service_instance_name = ibm_resource_instance.registry.name
-     namespace_id          = var.unique_id
-     resource_group_id     = ibm_resource_group.resource_group.id
-     key                   = ibm_resource_key.registry_key.name
 }
-######################################################
